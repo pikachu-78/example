4f3ae266d457d68c2c8f2fa08fca206dfc473750
daniel-856 <134355514+daniel-856@users.noreply.github.com>
2024-06-03 10:05:32 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,25 +1,31 @@
 import json
 import argparse
-import sys
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--changed_files", default="conf/**", help="Space-separated list of changed JSON files")
+parser.add_argument("--changed_files", default="conf/**")
 args = parser.parse_args()
 changed_files = args.changed_files
 
-def main(file_path):
-    try:
-        with open(file_path) as json_file:
-            teams = json.load(json_file)
-            for team_data in teams:
-                print(team_data['name'])
-                print(team_data['roll'])
-    except FileNotFoundError:
-        print(f"File {file_path} not found")
-    except json.JSONDecodeError:
-        print(f"File {file_path} is not a valid JSON file")
+def main(file_paths=None):
+    if file_paths is None:
+        print("No file paths provided.")
+        return
+    for file_path in file_paths:
+        try:
+            with open(file_path) as json_file:
+                teams = json.load(json_file)
+                for team_data in teams:
+                    print(team_data['name'])
+                    print(team_data['roll'])
+        except FileNotFoundError:
+            print(f"File {file_path} not found")
+        except json.JSONDecodeError:
+            print(f"File {file_path} is not a valid JSON file")
 
 
 if __name__ == "__main__":
-    main(changed_files)
+    if len(changed_files) != 1:
+        main()
+    else:
+        main(changed_files)
