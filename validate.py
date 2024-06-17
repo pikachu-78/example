d9e48513eec8b917bca92a05b58b632f0f393103
daniel-856 <134355514+daniel-856@users.noreply.github.com>
2024-06-17 12:18:33 +0530

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,5 +1,13 @@
 import json
 import jsonschema
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--changed_file")  # Expecting a single comma-separated string
+args = parser.parse_args()
+changed_files = args.changed_file.split()  # Split the input string on whitespace
+
+print("Changed files:", changed_files)  
 
 # Custom validation function for 'isNotEmpty'
 def validate_is_not_empty(validator, isNotEmpty, instance, schema):
@@ -22,14 +30,19 @@ def validate_json(json_data, schema_data):
         print(e)
         return False
 
-def load_json(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
+def load_json(file_paths):
+    for file_path in file_paths:
+        try:
+            with open(file_path) as json_file:
+                return json.load(file)
+        except FileNotFoundError:
+            print(f"File {file_path} not found")
+        except json.JSONDecodeError:
+            print(f"File {file_path} is not a valid JSON file")
 
 def main():
     # Load JSON data
-    json_file_path = 'copy.json'
-    json_data = load_json(json_file_path)
+    json_data = load_json(changed_files)
 
     # Load JSON schema
     schema_file_path = 'schema.json'
