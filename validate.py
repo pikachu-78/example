5a763e630a13dfa064c8906716550149546de0ef
daniel-856 <134355514+daniel-856@users.noreply.github.com>
2024-06-17 14:28:56 +0530

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,12 +1,12 @@
 import json
 import jsonschema
 import argparse
-import sys
+import sys 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--changed_file")  # Expecting a single comma-separated string
 args = parser.parse_args()
-changed_files = args.changed_file.split()  # Split the input string on whitespace
+changed_files = args.changed_file  # Split the input string on whitespace
 
 print("Changed files:", changed_files)  
 
@@ -23,56 +23,42 @@ jsonschema.Draft7Validator.VALIDATORS['isNotEmpty'] = validate_is_not_empty
 def validate_json(json_data, schema_data):
     try:
         validator = jsonschema.Draft7Validator(schema_data)
-        for obj in json_data:
-            validator.validate(obj)
+        validator.validate(json_data)
         print("JSON is valid against the schema.")
         return True
     except jsonschema.exceptions.ValidationError as e:
-        raise e
+        raise e 
 
 def load_json(file_paths):
-    loaded_data = []
     for file_path in file_paths:
         try:
             print(file_path)
             with open(file_path) as json_file:
-                loaded_data.append(json.load(json_file))
+                return json.load(json_file)
         except FileNotFoundError:
             print(f"File {file_path} not found")
         except json.JSONDecodeError:
             print(f"File {file_path} is not a valid JSON file")
-    
-    return loaded_data
 
 def loads_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
 def main():
+    # Load JSON data
+    json_data = load_json(changed_files)
+
     # Load JSON schema
     schema_file_path = 'schema.json'
     schema_data = loads_json(schema_file_path)
-    
-    all_valid = True  # Track overall validation result
-    
-    # Validate each JSON file against the schema
-    for file_path in changed_files:
-        json_data = load_json([file_path])
-        
-        if json_data is None:
-            all_valid = False
-            continue
-        
-        try:
-            validate_json(json_data, schema_data)
-            print(f"{file_path}: JSON is valid against the schema.")
-        except jsonschema.exceptions.ValidationError as e:
-            print(f"{file_path}: JSON is not valid against the schema.")
-            print(e)
-            all_valid = False
-    
-    if not all_valid:
-        sys.exit(1)  # Exit with failure status code if any validation fails
+
+    # Validate JSON against the schema
+    try:
+        validate_json(json_data, schema_data)
+    except jsonschema.exceptions.ValidationError as e:
+        print("JSON is not valid against the schema.")
+        print(e)
+        sys.exit(1)  # Exit with failure status code
 
 if __name__ == "__main__":
     main()
